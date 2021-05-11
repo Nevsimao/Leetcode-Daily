4313bb07744c6e571a436c1324fa99151a85fd2c
Simao Neves <78128636+Nevsimao03@users.noreply.github.com>
2021-05-11 16:25:58 -0400

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,7 @@
+### Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            nums[i] *= nums[i]
+        return sorted(nums)
