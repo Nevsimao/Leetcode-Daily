8ca36e2b17668924bcbf8d1fe7e76a7a6f59adbd
Simao Neves <78128636+Nevsimao03@users.noreply.github.com>
2021-05-14 13:21:57 -0400

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -3,3 +3,14 @@
 ## Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
 ### Solution (Python)
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = {} #using a dic to comper using keys
+        for i in strs:
+            key = ''.join(sorted(list(i))) #sort it, conver to a list and then join it back to string format
+            if key in result:
+                result[key].append(i) #if sorted strigs are the same it can be append them to the dic
+            else:
+                result[key] = [i]
+        return result.values()
