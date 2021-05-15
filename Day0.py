47214f34b7bdcc64e96864e20d852e50a08ca268
Simao Neves <78128636+Nevsimao03@users.noreply.github.com>
2021-05-14 22:03:58 -0400

    Update Day0.py

--- Day0.py
+++ Day0.py
@@ -1,16 +1,5 @@
-###Given an array of integers arr, return true if and only if it is a valid mountain array.
+# 3Sum
 
-class Solution:
-    def validMountainArray(self, arr: List[int]) -> bool:
-        if len(arr) < 3:
-            return False
-        pointer = 0
-        for i in range(len(arr)-1):
-            if arr[i] == arr[i+1]:
-                return False
-            if pointer == 0:
-                if arr[i] > arr[i+1]: pointer = 1
-            else:
-                if arr[i] <= arr[i+1]:
-                    return False
-        return True if arr[-2] > arr[-1] and arr[0] < arr[1] else False
+## Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+### Solution (Python)
