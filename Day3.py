0210bc64bb5d5d2ff85f49f2622f861681ca1552
Simao Neves <78128636+Nevsimao03@users.noreply.github.com>
2021-05-14 22:44:00 -0400

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -4,3 +4,18 @@
 
 ### Solution (Python)
 
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        right, left, s_len = {}, 0, 0
+        for i in range(0, len(s)):
+            # get the last index s[i]
+            # update the right index
+            # get maximum of last and right
+            # at that index add 1
+            if s[i] in right:
+                left = max(left, right[s[i]] + 1)
+            # update the result
+            s_len = max(s_len, i - left + 1)
+            # update right index
+            right[s[i]] = i
+        return s_len
