93f8e9a57b0f4b8633c79f6eef894300632d188b
Simao Neves <78128636+Nevsimao03@users.noreply.github.com>
2021-05-16 17:47:17 -0400

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -1,6 +1,6 @@
-# 
+# Increasing Triplet Subsequence
 
-## 
+## Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
 
 ### Solution (Python)
 
