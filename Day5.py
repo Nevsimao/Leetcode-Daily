0bfb77d3de4d0a3fdd3d7c82d691caa2157faaa4
Simao Neves <78128636+Nevsimao03@users.noreply.github.com>
2021-05-16 17:46:43 -0400

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -3,3 +3,16 @@
 ## 
 
 ### Solution (Python)
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        sNum = s_Num = sys.maxsize
+        for num in nums:
+            if num <= sNum:
+                sNum = num
+            elif num <= s_Num:
+                s_Num = num
+            else:
+                return True
+            
+        return False
