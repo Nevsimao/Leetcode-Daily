1211931b90a14953519ef47f901d4791ce001707
Simao Neves <78128636+Nevsimao03@users.noreply.github.com>
2021-05-17 16:11:45 -0400

    Update Day7.py

--- Day7.py
+++ Day7.py
@@ -4,3 +4,22 @@
 
 ### Solution (Python)
 
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        odd, even = ListNode(0), ListNode(0)
+        oddHead, evenHead = odd, even
+        current = 0
+        
+        while head:
+            if current & 1 == 0:
+                odd.next = head
+                odd = odd.next
+            else:
+                even.next = head
+                even = even.next
+            head = head.next
+            current += 1
+        even.next = None
+        odd.next = evenHead.next
+        
+        return oddHead.next
